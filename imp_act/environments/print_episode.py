21677c0e75241398f55c01fbd34af1b77bb34a46
Daniel Hettegger <daniel.hettegger@tum.de>
2024-03-21 17:57:08 +0100

    print episode features

--- imp_act/environments/print_episode.py
+++ imp_act/environments/print_episode.py
@@ -1,3 +1,8 @@
+import numpy as np
+
+import argparse
+
+
 from imp_act import make
 
 
@@ -28,28 +33,44 @@ def heuristic_policy(observation):
                 edge_actions.append(4)  # Reconstruction
             elif segment >= 5:
                 edge_actions.append(3)  # Major repair
-            elif segment >= 1:
+            elif segment >= 2:
                 edge_actions.append(2)  # Minor repair
-            elif current_time % 1 == 0:
+            elif current_time % 2 == 0:
                 edge_actions.append(1)  # Inspection
             else:
                 edge_actions.append(0)  # Do nothing
         actions.append(edge_actions)
     return actions
 
+def get_policy(policy_name):
+    if policy_name == "do_nothing":
+        return do_nothing_policy
+    elif policy_name == "fail_replace":
+        return fail_replace_policy
+    elif policy_name == "heuristic":
+        return heuristic_policy
+    else:
+        raise ValueError(f"Unknown policy: {policy_name}")
 
-def main():
-    env = make("Denmark-v1")
+def main(args):
+    env = make(args.env)
     obs = env.reset()
     done = False
     timestep = 0
     total_reward = 0
-    policy = heuristic_policy
+    total_travel_time_reward = 0
+    total_maintenance_reward = 0
+    total_normalized_delay = 0
+    policy = get_policy(args.policy)
     while not done:
         timestep += 1
         actions = policy(obs)
         obs, reward, done, info = env.step(actions)
         total_reward += reward
+        total_travel_time_reward += info["reward_elements"][0]
+        total_maintenance_reward += info["reward_elements"][1]
+
+        total_normalized_delay += info["normalized_delay"]
 
         print(f"timestep: {timestep}")
         print(f"reward: {reward:.2e}")
@@ -58,23 +79,39 @@ def main():
         print(f"total travel time: {info['total_travel_time']}")
         print(f"remaining budget: {obs['remaining_budget']:.2e}")
         print(f"remaining budget time: {obs['remaining_budget_years']}")
+        print(f"normalized delay: {info['normalized_delay']:.5f}")
 
-        """for i, observations, beliefs, states in zip(
-            range(len(observation["edge_observations"])),
-            observation["edge_observations"],
-            observation["edge_beliefs"],
-            info["states"],
-        ):
+        max_util = max([max(util) for util in obs["edge_traffic_utilization"]])
+        mean_util = np.mean([np.mean(util) for util in obs["edge_traffic_utilization"]])
+        print(f"max utilization: {max_util:.2f}")
+        print(f"mean utilization: {mean_util:.2f}")
 
-            print(f"\nedge: {i}")
-            print(f"states:       {states}")
-            print(f"observations: {observations}")
-            print(f"beliefs:      {[list(np.round(belief,2)) for belief in beliefs]}")"""
+        if args.print_segment_info:
+            for i, observations, beliefs, states, util in zip(
+                range(len(obs["edge_observations"])),
+                obs["edge_observations"],
+                obs["edge_beliefs"],
+                info["states"],
+                obs["edge_traffic_utilization"],
+            ):
+                print(f"\nedge: {i}")
+                print(f"states:       {states}")
+                print(f"observations: {observations}")
+                print(f"beliefs:      {[list(np.round(belief,2)) for belief in beliefs]}")
+                print(f"utilization:  {util}")
 
         print("=" * 50)
 
     print(f"total reward: {total_reward:.3e}")
+    print(f"total travel time reward: {total_travel_time_reward:.3e}")
+    print(f"total maintenance reward: {total_maintenance_reward:.3e}")
+    print(f"average normalized delay: {total_normalized_delay/timestep:.3f}")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", default="Denmark-v1")
+    parser.add_argument("--policy", default="heuristic")
+    parser.add_argument("--print-segment-info", "-si", action="store_true")
+    args = parser.parse_args()
+    main(args)
